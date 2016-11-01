65087710f69475275a498497ec6ce0b0a4c6eaee
Qiyuan Pu <pqy7172@gmail.com>
2016-11-01 12:24:20 +0800

    1001 problems

---
+++ 1001/1001.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int* convert( int* A, int s )
+{
+  int i = 0;
+  
+  while (i <= 5 && s != 0)
+    {
+      A[i] = s % 2;
+      s = s / 2;
+      i++;
+    }
+  return A;
+}
+
+int main(int argc, char *argv[])
+{
+  int* A = (int*)malloc(6 * sizeof(int));
+  int i, j ,k;
+
+  for ( i = 0; i <= 63; i++ )
+    {
+      for ( k = 0; k <= 5; k++)
+	A[k] = 0;
+      convert( A, i );
+      for ( j = 5; j >= 0; j-- )
+	printf("%d",A[j]);
+      printf("\n");
+    }
+  return 0;
+}
